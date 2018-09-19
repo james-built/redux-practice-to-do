55fb43da7e5aebc0992e611f4c292723efe7beee
james-built <jgbuilt@gmail.com>
2018-09-19 15:01:51 +1200

    changed public folder location & dependent pathing in server

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,10 @@
-const path = require('path')
 const express = require('express')
 
 const routes = require('./routes/home')
 
 const server = express()
 
-server.use(express.static(path.join(__dirname, 'public')))
+server.use(express.static('public'))
 
 server.use('/', routes)
 
